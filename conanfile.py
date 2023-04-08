133a16097a3cb6cd1aef413fc4e71236960ecf2f
vvvar <voinovvladv@gmail.com>
2023-04-08 13:05:16 +0200

    Prototype of packaging

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conan import ConanFile
+from conan.tools.scm import Git
+from conan.tools.files import rmdir, copy
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+import os
+
+class Pluginval(ConanFile):
+    name="pluginval"
+    version="1.0.3"
+    channel="release"
+    user="pluginval"
+    author="pluginval"
+    homepage="https://github.com/Tracktion/pluginval"
+    url="https://github.com/Tracktion/pluginval.git"
+    description="Cross platform plugin testing and validation tool"
+    license="https://github.com/Tracktion/pluginval/blob/master/LICENSE"
+
+    settings="os", "compiler", "arch", "build_type"
+    package_type = "application"
+    exports_sources = "*", "!.vscode", "!build"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def source(self):
+        rmdir(self, "pluginval")
+        Git(self).run(f"clone --recurse-submodules --depth 1 --branch v{self.version} {self.url}")
+
+
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.generate()
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=os.path.join(str(self.source_folder), "pluginval"))
+        cmake.build()
+
+    def package(self):
+        copy(self, "*", src=os.path.join(str(self.build_folder), "pluginval_artefacts", "Release"), dst=os.path.join(str(self.package_folder), "bin"))
+
+    def package_info(self):
+        if self.settings.os == "Macos":
+            self.buildenv_info.append_path("PATH", os.path.join(str(self.package_folder), "bin", "pluginval.app", "Contents", "MacOS"))
+        else:
+            self.buildenv_info.append_path("PATH", os.path.join(str(self.package_folder), "bin"))
